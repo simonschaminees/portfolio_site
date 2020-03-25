95be3b335ac49f8d28964171b2a5428de2d8bff0
simon schaminee <simon.schaminee@hotmail.com>
2020-03-25 09:31:54 +0100

    trying to add sass loaders

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
+// vue.config.js
 module.exports = {
-    configureWebpack: config => {
-      if (process.env.NODE_ENV === 'production') {
-        // mutate config for production...
-      } else {
-        // mutate for development...
-      }
+    configureWebpack: {
+      plugins: [
+        //new MyAwesomeWebpackPlugin()
+      ]
     }
   }
