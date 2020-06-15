d1397e3a6db296ff536a84208aed6b424b98802a
simonschaminees <simon.schaminee@hotmail.com>
2020-06-15 20:17:34 +0200

    installed bootstrap vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="hello">
+    <b-alert show variant="primary">Primary Alert</b-alert>
+  <b-alert show variant="secondary">Secondary Alert</b-alert>
+  <b-alert show variant="success">Success Alert</b-alert>
+  <b-alert show variant="danger">Danger Alert</b-alert>
+  <b-alert show variant="warning">Warning Alert</b-alert>
+  <b-alert show variant="info">Info Alert</b-alert>
+  <b-alert show variant="light">Light Alert</b-alert>
+  <b-alert show variant="dark">Dark Alert</b-alert>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import '../src/scss/main.scss';
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
