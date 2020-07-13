8ed72b2534733ec3b78ac721fca72e373f6baec9
simonschaminees <simon.schaminee@hotmail.com>
2020-07-13 20:15:36 +0200

    beginning of nav

---
+++ src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar>
+    <b-navbar-brand>
+       <img class="mt-4 mb-4" alt="Logo Simon" src="../assets/images/logo-simon.svg">
+    </b-navbar-brand>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Over mij</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<b-container fluid>
+  <b-row class=lichtgrijs>
+    <b-col cols="10" offset-lg="1">
+      <navigation/>
+    </b-col>
+ </b-row>
+ <b-row>
+   <h1>test</h1>
+ </b-row>
+
+
+   
+
+</b-container>    
 </template>
+
+<script>
+import navigation from '../components/Navigation.vue'
+export default {
+  name: 'About',
+  components: {
+    navigation
+  }
+}
+</script>
+
