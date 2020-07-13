12257501fe60753b4711fa7090aac2da5d41e018
simonschaminees <simon.schaminee@hotmail.com>
2020-07-13 20:32:38 +0200

    first setup for page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,13 +5,23 @@
       <navigation/>
     </b-col>
  </b-row>
- <b-row>
-   <h1>test</h1>
+ <b-row class=lichtgrijs>
+    <b-col cols="5" offset-lg="1">
+     <h1 class="mainTitle">Welkom</h1>
+    <p  class="payOff">De verlosser, 
+      De oppergeneraal 
+      De redder van dx2
+De wcm goeroe, Eerlijkheid boven alles. </p>
+   </b-col>
+   <b-col cols="5">
+    <img alt="Simon" src="../assets/images/PV-SimonSchaminee-transperant.png">
+   </b-col>
  </b-row>
-
-
-   
-
+ <b-row>
+   <b-col>
+     
+   </b-col>
+ </b-row>
 </b-container>    
 </template>
 
