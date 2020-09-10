cf824160b43f4566d2d85e67e066d6439b0f0e75
simonschaminees <simon.schaminee@hotmail.com>
2020-09-10 13:09:16 +0200

    ftp test

--- ftpdeploy/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  localBasePath:"/",
-  remoteBasePath: "/webspace/httpdocs/simonschaminee.nl",
-  sync: [
-    { src: "/dist", dest: "/webspace/httpdocs/simonschaminee.nl" }
-  ]
-};
