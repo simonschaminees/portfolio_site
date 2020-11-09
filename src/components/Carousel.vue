c90543e8fc2d2c7e7b376e086de6ac279a90f049
simonschaminees <simon.schaminee@hotmail.com>
2020-11-09 20:51:07 +0100

    updated for swiper carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,33 +1,35 @@
 <template>
-  <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
+  <swiper ref="mySwiper" :options="swiperOptions">
     <swiper-slide>Slide 1</swiper-slide>
     <swiper-slide>Slide 2</swiper-slide>
     <swiper-slide>Slide 3</swiper-slide>
-    ...
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </template>
-<script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper styles
+<script>
   export default {
-    components: {
-      Swiper,
-      SwiperSlide,
+    name: 'carrousel',
+    data() {
+      return {
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          // Some Swiper option/callback...
+        }
+      }
     },
-    methods: {
-      onSwiper(swiper) {
-        console.log(swiper)
-      },
-      onSlideChange() {
-        console.log('slide change')
-      },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
     },
-  };
+    mounted() {
+      console.log('Current Swiper instance object', this.swiper)
+      this.swiper.slideTo(3, 1000, false)
+    }
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import '../src/scss/main.scss';
+import 'swiper/swiper-bundle.css'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -10,6 +12,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 Vue.config.productionTip = false
 
